f04e0f9585b41a9d1c0ba640849fad3e23add8fc
sarabehnoodi <sbehnoodi@yahoo.com>
2020-01-31 07:57:16 -0800

    add ticketLockInit and ticketLockTest systemcalls

--- xv6-public/user.h
+++ xv6-public/user.h
@@ -23,6 +23,10 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
+int ticketLockInit(void);
+int ticketLockTest(void);
+
+
 
 // ulib.c
 int stat(const char*, struct stat*);
